aa9cf61b9d8cfd116ce6d577203f22a0469f8b97
Andrew Morgan <andrewdmorgan.2@gmail.com>
2023-09-25 15:48:06 +0100

    Where I am leaving it
    So until the models get better there is no point in continuing. Needs to be really good generation for the tool to  be good

--- backend/googleColabSetup.py
+++ backend/googleColabSetup.py
@@ -1,13 +1,1 @@
-from selenium import webdriver
-
-# Set up the WebDriver
-driver = webdriver.Chrome()  # Or choose another WebDriver
-
-# Log in to Google Colab and navigate to your notebook
-driver.get('https://colab.research.google.com/drive/19sKlgMz3tv9AcibClvdhmoIlboT9ZXtr#scrollTo=nrYti-FEs9kl')
-# Perform login if required
-# Open your Colab notebook
-
-# Use Selenium to send data to cells and execute them
-code_to_execute = "print('Hello from Colab!')"
-driver.execute_script(f"google.colab.kernel.invokeFunction('notebook.runCell', ['code', {{\"cellId\":\"your_cell_id\", \"code\":\"{code_to_execute}\"}}])")
+from colabcode import ColabCode
